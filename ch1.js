6621c3934e9f3415cd968b22867dc39bfc6926bb
Shafe Ahsan <safeahsan786@gmail.com>
2024-01-25 12:58:24 +0530

    my message

---
+++ ch1.js
@@ -0,0 +1,74 @@
+// let fruit = [1,"banana","orange"];
+// console.log(fruit[0]);
+// let example = "Hello";
+// console.log(typeof(example));
+// let result = 5 - "5";
+// console.log(result);
+// console.log(typeof(result));
+// let m = "2" * "5" *"2";
+// console.log(typeof(m));
+// console.log(m);
+// let num = 10;
+// // let y = 5;
+// // console.log(x+y);
+// // console.log(x-y);
+// // console.log(x*y);
+// // console.log(x/y);
+// // console.log(x%y);
+// // console.log(x>y);
+// // console.log(x<y);
+// // console.log(x>=y);
+// // console.log(x<=y);
+// // console.log(x===y);
+// // console.log(x&&y);
+// // console.log(x||y);
+// // console.log(!y);
+// num+=5;
+// console.log(num);
+// num-=3;
+// console.log(num);
+// num*=2;
+// console.log(num);
+// num/=4
+// console.log(num);
+// for(let i=0;i<5;i++){
+//     console.log(i);
+// }
+// let count = 0;
+// while(count<5){
+//     console.log(count++);
+// }
+// let count = 0;
+// do{
+//     console.log(count++);
+// }while(count<5);
+// let person = {name: "john",age:30,occupation:"developement"};
+// for(let key in person){
+//     console.log(key + ": " + person[key]);
+// }
+// let car = {
+//     make:"toyota";
+//     model:"camry";
+//     year:2022;
+//     isElectric = false;
+//     start : function myfunc(){
+//         console.log("Engine has started");
+   
+//     }
+//     console.log(car.start);
+// }
+// function book(title,author,year){
+//     this.title = title;
+
+//     this.author = author;
+//     this.year = year;
+// }
+// let fruits = ["apple","banana","orange","grape"];
+// fruits.push("kiwi");
+// fruits.unshift("mango");
+// // fruits.shift();
+// // fruits.pop();
+// for(let key in fruits){
+//     console.log(fruits.indexOf("orange"));
+// }
+let name = "ApnaCollege";
